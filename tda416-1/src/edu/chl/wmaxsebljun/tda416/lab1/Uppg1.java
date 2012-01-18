c379803296da760c1f933f8772b98959e37dc1e9
kirayatail <kirayatail@gmail.com>
2012-01-18 12:00:04 +0100

    created method structures, implemented shift method

--- tda416-1/src/edu/chl/wmaxsebljun/tda416/lab1/Uppg1.java
+++ tda416-1/src/edu/chl/wmaxsebljun/tda416/lab1/Uppg1.java
@@ -2,4 +2,53 @@ package edu.chl.wmaxsebljun.tda416.lab1;
 
 public class Uppg1 {
 
+	private String[] elements;
+	private int size;
+
+	public Uppg1() {
+		this(20);
+	}
+
+	public Uppg1(int capacity) {
+		elements = new String[capacity];
+		size = 0;
+	}
+
+	public void addFirst(String element) {
+		// TODO
+	}
+
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+
+	public String getFirst() {
+		// TODO
+		return "";
+	}
+
+	public void removeFirst() {
+		this.shift(-1);
+		this.size--;
+	}
+
+	public boolean exist(String element) {
+		// TODO
+		return false;
+	}
+
+	public String toString() {
+		// TODO
+		return "";
+	}
+
+	private void shift(int offset) {
+		String[] temp = new String[this.elements.length];
+		for(int i = 0; i < this.elements.length; i++) {
+			if(i + offset >= 0 && i + offset < this.elements.length) {
+				temp[i + offset] = this.elements[i];
+			}
+		}
+		this.elements = temp;
+	}
 }
