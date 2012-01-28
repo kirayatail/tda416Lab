84eba852c384913a28e959a51fc46b66939cbebe
kirayatail <kirayatail@gmail.com>
2012-01-28 14:07:08 +0100

    Begin finalization of lab1 exc2

--- tda416-1/src/RecursiveMath.java
+++ tda416-1/src/RecursiveMath.java
@@ -1,29 +1,50 @@
-
-
+/**
+ * Problem set 1, exercise 2
+ * 
+ * 
+ * 
+ * 
+ * @author Sebastian Ljunggren, Max Witt
+ * 
+ */
 public class RecursiveMath {
 
 	public static void main(String[] args) {
 		System.out.println(binarySqrt(81, 0.000001));
 		System.out.println(binarySqrt(9, 0.000001));
-		System.out.println(binarySqrt(102400,0.000001));
+		System.out.println(binarySqrt(102400, 0.000001));
+		
+		try{
+			System.out.println("FAIL! faulty root of 1: "+binarySqrt(1,0.001));
+		} catch(IllegalArgumentException e){
+			System.out.println("OK! Exception for sqr == 1 caught");
+		}
+		try{
+			System.out.println("FAIL! Computed inacceptable eps 0: "+binarySqrt(9,0));
+		} catch(IllegalArgumentException e){
+			System.out.println("OK! Exception for eps == 0 caught");
+		}
 	}
 
 	/**
 	 * Finds square root by binary search. This is the access method, and
 	 * therefore public.
+	 * The precision limit determines that the result must be within ± <code>eps</code> of the true root
 	 * 
 	 * @param sqr
+	 *           - number to find the root of, must be >1
 	 * @param eps
+	 *           - precision limit, must be >0
 	 * @return
-	 * @throws IllegalArgumentException
+	 * @throws IllegalArgumentException thrown when parameter rules are violated
 	 */
 	public static double binarySqrt(double sqr, double eps)
 			throws IllegalArgumentException {
-		
-		if (sqr < 1) {
+
+		if (sqr <= 1) {
 			throw new IllegalArgumentException("Argument sqr must be >1");
 
-		} else if (eps < 0) {
+		} else if (eps <= 0) {
 			throw new IllegalArgumentException("Argument eps must be >0");
 		} else {
 			return help(sqr, eps, 1, sqr);
@@ -31,17 +52,17 @@ public class RecursiveMath {
 	}
 
 	/**
-	 * This is the recursive help method that does all work. Does no validation,
-	 * (which is done in the access method) and is private for protection.
+	 * This is the recursive help method that does all work. Does no input validation
+	 * (which is done in the access method).
 	 * 
-	 * @param sqr
+	 * @param sqr 
 	 * @param eps
 	 * @param low
 	 * @param high
 	 * @return
 	 */
 	private static double help(double sqr, double eps, double low, double high) {
-		
+
 		double candidate = (high + low) / 2;
 		if (Math.abs(candidate * candidate - sqr) < eps) {
 			return candidate;
