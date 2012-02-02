6babc9de17b55e75fbd98defaca97c33a1fe723c
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-02-02 14:44:58 +0100

    Implmented get() and added javadoc.

--- 10.lab2/src/SortedLinkedCollection.java
+++ 10.lab2/src/SortedLinkedCollection.java
@@ -1,28 +1,70 @@
+import testSortCol.CollectionWithGet;
 import datastructures.LinkedCollection;
+import java.util.*;
 
+public class SortedLinkedCollection<E extends Comparable<E>> extends
+		LinkedCollection<E> implements CollectionWithGet<E> {
 
-public class SortedLinkedCollection<E extends Comparable<E>> extends LinkedCollection<E> implements CollectionWithGet{
-	
-	public SortedLinkedCollection(){
+	public SortedLinkedCollection() {
 		super();
 	}
-	
+
+	/**
+	 * Adds an element to the collection. The method makes sure it is added in
+	 * the correct position. Duplicates are allowed.
+	 * 
+	 * @param element
+	 *            the element to add
+	 * @return <code>true</code> if the element was successfully added.
+	 * @throws IllegalArgumentException
+	 *             if the element is <code>null</code>
+	 */
 	@Override
-	public boolean add(E element){
-		if(element == null){
+	public boolean add(E element) throws IllegalArgumentException {
+		if (element == null) {
 			throw new IllegalArgumentException("Argument cannot be null");
 		}
 		Entry current = this.head;
-		if(head == null){
+		if (head == null) {
 			head = new Entry(element, null);
 		} else {
-			
-			while(current.next != null && element.compareTo(current.next.element) <=0){
+
+			while (current.next != null
+					&& element.compareTo(current.next.element) <= 0) {
 				current = current.next;
 			}
 			current.next = new Entry(element, current.next);
 		}
-		
+
 		return true;
+	}
+
+	/**
+	 * Looks for the first occurrence of an element in the collection and return
+	 * it if it is in the collection.
+	 * 
+	 * @param element
+	 *            the element to look in the collection for
+	 * @return a reference to the element in the collection or <code>null</code>
+	 *         if it isn't found
+	 */
+	@Override
+	public E get(E element) {
+		if (element == null) {
+			return null;
+		}
+		/*
+		 * Vi pratade med Niklas om att använda iteratorn och han tyckte det var
+		 * ok.
+		 */
+		Iterator<E> i = this.iterator();
+		E inList;
+		while (i.hasNext()) {
+			inList = i.next();
+			if (inList.compareTo(element) == 0) {
+				return inList;
+			}
+		}
+		return null;
 	}
 }
