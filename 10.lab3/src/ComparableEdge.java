0cb539ecd609dc4de81b1aaa7563e05f48a11425
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2012-02-20 13:23:34 +0100

    Added ComparableEdge and KruskalEdge.

---
+++ 10.lab3/src/ComparableEdge.java
@@ -0,0 +1,15 @@
+
+public class ComparableEdge extends Edge {
+
+	public ComparableEdge(int from, int to) {
+		super(from, to);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ 10.lab3/src/KruskalEdge.java
@@ -0,0 +1,15 @@
+
+public class KruskalEdge extends Edge {
+
+	public KruskalEdge(int from, int to) {
+		super(from, to);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
