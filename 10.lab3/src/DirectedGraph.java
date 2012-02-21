830c9a30eecd2eb3cdbd83d716a6ff320d30c01e
kirayatail <kirayatail@gmail.com>
2012-02-21 18:19:25 +0100

    Bugfixes and made sure Kruskal's algorithm works per specification.

--- 10.lab3/src/DirectedGraph.java
+++ 10.lab3/src/DirectedGraph.java
@@ -23,46 +23,95 @@ public class DirectedGraph<E extends Edge> {
 	public Iterator<E> shortestPath(int from, int to) {
 		return null;
 	}
+
 	/**
 	 * Woho! Kruskal!
+	 * 
 	 * @return
 	 */
 	public Iterator<E> minimumSpanningTree() {
-		
+		// Create and populate PQ
 		PriorityQueue<E> pq = new PriorityQueue<E>();
 		Iterator<E> it = edgeList.iterator();
-		while(it.hasNext()){
+		while (it.hasNext()) {
 			pq.add(it.next());
 		}
-		List<E>[] nodeArray = (List<E>[]) new LinkedList[this.noNodes];
-		for(int i=0; i<nodeArray.length; i++){
+
+		// Create node array (each index represents a node number) which will
+		// contain empty lists of edges (components)
+		List<E>[] nodeArray = (List<E>[]) new LinkedList[this.noNodes+1];
+		for (int i = 0; i < nodeArray.length; i++) {
 			nodeArray[i] = new LinkedList<E>();
 		}
-		
-		while(!pq.isEmpty()){
+
+		while (!pq.isEmpty()) {
+
+			// Get the lightest edge
 			E candEdge = pq.poll();
-			int small, big;
-			if(nodeArray[candEdge.from] != nodeArray[candEdge.to]){
-				if(nodeArray[candEdge.from].size() >= nodeArray[candEdge.to].size()){
+
+			int small, big; // indices referring to which of the from-to pair
+							// has the smallest/largest component
+
+			// Doesn't point to the same component, do the magic!
+			if (nodeArray[candEdge.from] != nodeArray[candEdge.to]) {
+
+				// Determine and set big and small
+				if (nodeArray[candEdge.from].size() >= nodeArray[candEdge.to]
+						.size()) {
 					big = candEdge.from;
 					small = candEdge.to;
 				} else {
 					big = candEdge.to;
 					small = candEdge.from;
 				}
-				
-				for(E subEdge : nodeArray[small]){
+
+				// Re-point each from and to from each edge in the "small"
+				// component and also add each edge to "big"
+				for (E subEdge : nodeArray[small]) {
 					nodeArray[subEdge.from] = nodeArray[big];
 					nodeArray[subEdge.to] = nodeArray[big];
 					nodeArray[big].add(subEdge);
 				}
+
+				// Re-point small to point at big's component.
 				nodeArray[small] = nodeArray[big];
-				if(nodeArray[big].size() == nodeArray.length-1){
+
+				// Add the candidate to "big"
+				nodeArray[big].add(candEdge);
+				
+				// We're done if big's component has n-1 edges (where n = number
+				// of nodes)
+				if (nodeArray[big].size() == (nodeArray.length - 2)) {
 					return nodeArray[big].iterator();
 				}
-			}	
+			}
 		}
 		return null;
+	}
+	
+	public static void main(String[] args){
+		
+		// Example from lecture
+		
+		DirectedGraph<KruskalEdge> dg = new DirectedGraph<KruskalEdge>(6); 
+		
+		dg.addEdge(new KruskalEdge(5,6,6));
+		dg.addEdge(new KruskalEdge(2,5,3));
+		dg.addEdge(new KruskalEdge(3,5,6));
+		dg.addEdge(new KruskalEdge(3,6,4));
+		dg.addEdge(new KruskalEdge(4,6,2));
+		dg.addEdge(new KruskalEdge(2,3,5));
+		dg.addEdge(new KruskalEdge(3,4,5));
+		dg.addEdge(new KruskalEdge(1,3,1));
+		dg.addEdge(new KruskalEdge(1,2,6));
+		dg.addEdge(new KruskalEdge(1,4,5));
+		
+		Iterator<KruskalEdge> it = dg.minimumSpanningTree();
+		
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
+		
 	}
 
 }
