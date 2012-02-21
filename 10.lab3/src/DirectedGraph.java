732eb7fe98a7ba852f8cb538a46e06d2c41a5113
kirayatail <kirayatail@gmail.com>
2012-02-21 08:18:02 +0100

    surrounding structure of the graph - an arraylist with linked lists within

--- 10.lab3/src/DirectedGraph.java
+++ 10.lab3/src/DirectedGraph.java
@@ -1,11 +1,16 @@
-
 import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
 
+	/*
+	 * Simple array containing linked lists with edges. Every index of the array
+	 * corresponds to a node, with the directive edges as content
+	 */
+	private List<LinkedCollection<E>> nodeList;
 
 	public DirectedGraph(int noOfNodes) {
-		;
+		nodeList = new ArrayList<LinkedCollection<E>>(noOfNodes); 
+		
 	}
 
 	public void addEdge(E e) {
@@ -15,10 +20,9 @@ public class DirectedGraph<E extends Edge> {
 	public Iterator<E> shortestPath(int from, int to) {
 		return null;
 	}
-		
+
 	public Iterator<E> minimumSpanningTree() {
 		return null;
 	}
 
 }
-  
