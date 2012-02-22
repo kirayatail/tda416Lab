073a81e22784b3faa2fe0f7cde64a194912dee1b
kirayatail <kirayatail@gmail.com>
2012-02-22 14:49:06 +0100

    Remake KruskalEdge and change DG-test to reflect actual use

--- 10.lab3/src/DirectedGraph.java
+++ 10.lab3/src/DirectedGraph.java
@@ -28,48 +28,48 @@ public class DirectedGraph<E extends Edge> {
 	 * 
 	 * @return
 	 */
-	public Iterator<E> minimumSpanningTree() {
+	public Iterator<KruskalEdge<E>> minimumSpanningTree() {
 		// Create and populate PQ
-		PriorityQueue<E> pq = new PriorityQueue<E>();
+		PriorityQueue<KruskalEdge<E>> pq = new PriorityQueue<KruskalEdge<E>>();
 		Iterator<E> it = edgeList.iterator();
 		while (it.hasNext()) {
-			pq.add(it.next());
+			pq.add(new KruskalEdge<E>(it.next()));
 		}
 
 		// Create node array (each index represents a node number) which will
 		// contain empty lists of edges (components)
 		@SuppressWarnings("unchecked")
-		List<E>[] nodeArray = (List<E>[]) new LinkedList[this.noNodes+1];
+		List<KruskalEdge<E>>[] nodeArray = (List<KruskalEdge<E>>[]) new LinkedList[this.noNodes];
 		for (int i = 0; i < nodeArray.length; i++) {
-			nodeArray[i] = new LinkedList<E>();
+			nodeArray[i] = new LinkedList<KruskalEdge<E>>();
 		}
 
 		while (!pq.isEmpty()) {
 
 			// Get the lightest edge
-			E candEdge = pq.poll();
+			KruskalEdge<E> candEdge = pq.poll();
 
 			int small, big; // indices referring to which of the from-to pair
 							// has the smallest/largest component
 
 			// Doesn't point to the same component, do the magic!
-			if (nodeArray[candEdge.from] != nodeArray[candEdge.to]) {
+			if (nodeArray[candEdge.getFrom()] != nodeArray[candEdge.getTo()]) {
 
 				// Determine and set big and small
-				if (nodeArray[candEdge.from].size() >= nodeArray[candEdge.to]
+				if (nodeArray[candEdge.getFrom()].size() >= nodeArray[candEdge.getTo()]
 						.size()) {
-					big = candEdge.from;
-					small = candEdge.to;
+					big = candEdge.getFrom();
+					small = candEdge.getTo();
 				} else {
-					big = candEdge.to;
-					small = candEdge.from;
+					big = candEdge.getTo();
+					small = candEdge.getFrom();
 				}
 
 				// Re-point each from and to from each edge in the "small"
 				// component and also add each edge to "big"
-				for (E subEdge : nodeArray[small]) {
-					nodeArray[subEdge.from] = nodeArray[big];
-					nodeArray[subEdge.to] = nodeArray[big];
+				for (KruskalEdge<E> subEdge : nodeArray[small]) {
+					nodeArray[subEdge.getFrom()] = nodeArray[big];
+					nodeArray[subEdge.getTo()] = nodeArray[big];
 					nodeArray[big].add(subEdge);
 				}
 
@@ -81,7 +81,7 @@ public class DirectedGraph<E extends Edge> {
 				
 				// We're done if big's component has n-1 edges (where n = number
 				// of nodes)
-				if (nodeArray[big].size() == (nodeArray.length - 2)) {
+				if (nodeArray[big].size() == (nodeArray.length - 1)) {
 					return nodeArray[big].iterator();
 				}
 			}
@@ -93,20 +93,20 @@ public class DirectedGraph<E extends Edge> {
 		
 		// Example from lecture
 		
-		DirectedGraph<KruskalEdge> dg = new DirectedGraph<KruskalEdge>(6); 
+		DirectedGraph<BusEdge> dg = new DirectedGraph<BusEdge>(6); 
 		
-		dg.addEdge(new KruskalEdge(5,6,6));
-		dg.addEdge(new KruskalEdge(2,5,3));
-		dg.addEdge(new KruskalEdge(3,5,6));
-		dg.addEdge(new KruskalEdge(3,6,4));
-		dg.addEdge(new KruskalEdge(4,6,2));
-		dg.addEdge(new KruskalEdge(2,3,5));
-		dg.addEdge(new KruskalEdge(3,4,5));
-		dg.addEdge(new KruskalEdge(1,3,1));
-		dg.addEdge(new KruskalEdge(1,2,6));
-		dg.addEdge(new KruskalEdge(1,4,5));
+		dg.addEdge(new BusEdge(4,5,6,""));
+		dg.addEdge(new BusEdge(1,4,3,""));
+		dg.addEdge(new BusEdge(2,4,6,""));
+		dg.addEdge(new BusEdge(2,5,4,""));
+		dg.addEdge(new BusEdge(3,5,2,""));
+		dg.addEdge(new BusEdge(1,2,5,""));
+		dg.addEdge(new BusEdge(2,3,5,""));
+		dg.addEdge(new BusEdge(0,2,1,""));
+		dg.addEdge(new BusEdge(0,1,6,""));
+		dg.addEdge(new BusEdge(0,3,5,""));
 		
-		Iterator<KruskalEdge> it = dg.minimumSpanningTree();
+		Iterator<KruskalEdge<BusEdge>> it = dg.minimumSpanningTree();
 		
 		while(it.hasNext()){
 			System.out.println(it.next());
--- 10.lab3/src/KruskalEdge.java
+++ 10.lab3/src/KruskalEdge.java
@@ -1,29 +1,35 @@
 
-public class KruskalEdge extends Edge implements Comparable<KruskalEdge> {
+public class KruskalEdge<E extends Edge> implements Comparable<KruskalEdge<E>> {
 
-	private final int weight;
-
-	public KruskalEdge(int from, int to, int weight) {
-		super(from, to);
-		this.weight = weight;
-	}
-
-	@Override
-	public double getWeight() {
-		return this.weight;
-	}
-
-	@Override
-	public int compareTo(KruskalEdge edge) {
-		return this.weight - edge.weight;
+	private E edge;
+	public KruskalEdge(E e) {
+		this.edge = e;
 	}
 	
+	public double getWeight(){
+		return this.edge.getWeight();
+	}
+	public int compareTo(KruskalEdge<E> otherEdge) {
+		return ((int)this.edge.getWeight()) - ((int)otherEdge.getWeight());
+	}
+	
+	public int getFrom(){
+		return this.edge.from;
+	}
+	
+	public int getTo(){
+		return this.edge.to;
+	}
 	@Override
 	public boolean equals(Object o){
 		if(o.getClass() == this.getClass()){
-			return (super.equals(o) && (((KruskalEdge) o).weight == this.weight));
+			return (super.equals(o) && (((KruskalEdge<E>) o).compareTo(this)==0));
 		} else {
 			return false;
 		}
+	}
+	
+	public String toString(){
+		return this.edge.toString();
 	}
 }
